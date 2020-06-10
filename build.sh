2a0974c449c163e7875bca4ad09593e65c7ef0a2
xujiyou <xujiyou555@gmail.com>
2020-06-10 13:58:22 +0800

    构建脚本

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-operator-sdk build registry.prod.bbdops.com/common/drift:v0.0.1
+operator-sdk build registry.prod.bbdops.com/common/drift:v0.0.3
+docker push registry.prod.bbdops.com/common/drift:v0.0.3
