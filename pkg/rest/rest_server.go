3e44460280ba959ec30228bccef41cf4d17e7813
xujiyou <xujiyou555@gmail.com>
2020-06-10 09:51:27 +0800

    去掉多余的注释

--- pkg/rest/rest_server.go
+++ pkg/rest/rest_server.go
@@ -46,7 +46,6 @@ func StartRestServer(m manager.Manager) {
 		MaxAge: 12 * time.Hour,
 	}))
 
-	// the jwt middleware
 	authMiddleware, err := jwt.New(&jwt.GinJWTMiddleware{
 		Realm:       "test zone",
 		Key:         []byte("secret key"),
@@ -98,23 +97,9 @@ func StartRestServer(m manager.Manager) {
 				"message": message,
 			})
 		},
-		// TokenLookup is a string in the form of "<source>:<name>" that is used
-		// to extract token from the request.
-		// Optional. Default value "header:Authorization".
-		// Possible values:
-		// - "header:<name>"
-		// - "query:<name>"
-		// - "cookie:<name>"
-		// - "param:<name>"
-		TokenLookup: "header: Authorization, query: token, cookie: jwt",
-		// TokenLookup: "query:token",
-		// TokenLookup: "cookie:token",
-
-		// TokenHeadName is a string in the header. Default value is "Bearer"
+		TokenLookup:   "header: Authorization, query: token, cookie: jwt",
 		TokenHeadName: "Bearer",
-
-		// TimeFunc provides the current time. You can override it to use another time value. This is useful for testing or if your server uses a different time zone than your tokens.
-		TimeFunc: time.Now,
+		TimeFunc:      time.Now,
 	})
 
 	if err != nil {
@@ -130,7 +115,6 @@ func StartRestServer(m manager.Manager) {
 	})
 
 	authApi := router.Group("/api")
-	// Refresh time can be longer than token timeout
 	authApi.GET("/refresh_token", authMiddleware.RefreshHandler)
 	authApi.Use(authMiddleware.MiddlewareFunc())
 	{
